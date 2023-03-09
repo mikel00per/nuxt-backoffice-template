edf899b6bde4ee3ac38bff869707f75516301115
Miguel Morillo <miguelmwilliams@gmail.com>
2023-03-09 19:33:32 +0100

    feat(vuetify): change init page

--- code/app.vue
+++ code/app.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <v-layout>
+    <v-navigation-drawer>
+      <v-list>
+        <v-list-item title="Navigation drawer"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar title="Application bar"></v-app-bar>
+
+    <v-main style="min-height: 300px;"></v-main>
+  </v-layout>
 </template>
